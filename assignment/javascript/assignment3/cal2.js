6b22a163f3af1367d5825d40853f2a1d66924ee6
Aishwarya <aishwaryajorve1604@gmail.com>
2020-09-01 17:53:06 +0530

    assignment3 after changes

--- assignment/javascript/assignment3/cal2.js
+++ assignment/javascript/assignment3/cal2.js
@@ -1,32 +1,31 @@
 function add()
           {
-    let n1=parseFloat(document.getElementById("no1").value); 
-    let n2=parseFloat(document.getElementById("no2").value); 
+    let n1=parseFloat(document.getElementById("num1").value); 
+    let n2=parseFloat(document.getElementById("num2").value); 
     let r=n1+n2;
-    document.getElementById("ans").value=r;
-            
+    document.getElementById("answer").value=r;
           }
 
 function sub()
           {
-    let n1=parseFloat(document.getElementById("no1").value); 
-    let n2=parseFloat(document.getElementById("no2").value);  
+    let n1=parseFloat(document.getElementById("num1").value); 
+    let n2=parseFloat(document.getElementById("num2").value);  
     let r=n1-n2;
-    document.getElementById("ans").value=r;
+    document.getElementById("answer").value=r;
           }
 
 function mul()
           {
-    let n1=parseFloat(document.getElementById("no1").value); 
-    let n2=parseFloat(document.getElementById("no2").value); 
+    let n1=parseFloat(document.getElementById("num1").value); 
+    let n2=parseFloat(document.getElementById("num2").value); 
               let r=n1*n2;
-    document.getElementById("ans").value=r;
+    document.getElementById("answer").value=r;
           }
 
 function div()
           {
-    let n1=parseFloat(document.getElementById("no1").value); 
-    let n2=parseFloat(document.getElementById("no2").value); 
+    let n1=parseFloat(document.getElementById("num1").value); 
+    let n2=parseFloat(document.getElementById("num2").value); 
               let r=n1/n2;
-    document.getElementById("ans").value=r;
+    document.getElementById("answer").value=r;
           }
