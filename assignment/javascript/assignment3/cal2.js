7ad4c34e53813a218faa1443697c3d4db0ba9631
Aishwarya <aishwaryajorve1604@gmail.com>
2020-09-01 10:40:15 +0530

    assignment3

---
+++ assignment/javascript/assignment3/cal2.js
@@ -0,0 +1,32 @@
+function add()
+          {
+    let n1=parseFloat(document.getElementById("no1").value); 
+    let n2=parseFloat(document.getElementById("no2").value); 
+    let r=n1+n2;
+    document.getElementById("ans").value=r;
+            
+          }
+
+function sub()
+          {
+    let n1=parseFloat(document.getElementById("no1").value); 
+    let n2=parseFloat(document.getElementById("no2").value);  
+    let r=n1-n2;
+    document.getElementById("ans").value=r;
+          }
+
+function mul()
+          {
+    let n1=parseFloat(document.getElementById("no1").value); 
+    let n2=parseFloat(document.getElementById("no2").value); 
+              let r=n1*n2;
+    document.getElementById("ans").value=r;
+          }
+
+function div()
+          {
+    let n1=parseFloat(document.getElementById("no1").value); 
+    let n2=parseFloat(document.getElementById("no2").value); 
+              let r=n1/n2;
+    document.getElementById("ans").value=r;
+          }
